208678de4ca9a5404b6424582ed40da15fbda5fe
arjunherat <arjunherat12@hotmail.com>
2026-02-02 13:22:09 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 for (let index = 0; index < 4; index++) {
-    Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 78)
-    basic.pause(500)
-    Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 85)
-    Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 85)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 80)
+    basic.pause(1000)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 75)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 75)
     basic.pause(500)
 }
 Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
